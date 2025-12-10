ae1c8187b0c167f9c15597bbef1124f4b6d7a822
krez7 <karimcool5900@gmail.com>
2025-12-11 00:02:21 +0100

    test: correct assert's expected result in FEN constructor test

--- Tests/BitboardTest.cs
+++ Tests/BitboardTest.cs
@@ -35,7 +35,7 @@ public class BitboardTest
         		() => Assert.Equal((UInt64)Square.E8, testBoard.pieceBB[(int)Chessboard.Material.k]),
 			() => Assert.Equal((UInt64)Square.E8, testBoard.pieceBB[(int)Chessboard.Material.k]),
 			() => Assert.Equal(15, testBoard.castling),
-			() => Assert.Equal(36, testBoard.enPassant),
+			() => Assert.Equal(44, testBoard.enPassant),
 			() => Assert.Equal(0, testBoard.halfMoves)
     		);
 	}
